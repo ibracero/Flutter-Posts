ae7e242e44f6e07b4a91eddd6561dfe926153d97
Imanol Bracero <imanol.bracero@touchsurgery.com>
2019-07-18 12:49:03 +0100

    Added post details screen

--- lib/widget/detail.dart
+++ lib/widget/detail.dart
@@ -26,19 +26,46 @@ class _PostDetailScreenState extends State<PostDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: StreamBuilder<PostDetailsViewState>(
+        body: Stack(children: [
+      new SafeArea(
+          child: StreamBuilder<PostDetailsViewState>(
               stream: _postBloc.post,
               builder: (context, snapshot) {
                 if (snapshot.data is PostDetailsDataState) {
                   return _buildDetailsView(snapshot.data);
                 }
               })),
-    );
+      new AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      )
+    ]));
   }
 
   Widget _buildDetailsView(PostDetailsDataState data) {
     PostModel _post = data.postDetails;
-    return Text(_post.title)
+    return new Container(
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          getAvatar(_post),
+          new Padding(
+              padding: EdgeInsets.fromLTRB(16, 32, 16, 0),
+              child: new Text(
+                _post.title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              )),
+          new Padding(padding: EdgeInsets.all(16), child: new Text(_post.body))
+        ],
+      ),
+    );
+  }
+
+  Image getAvatar(PostModel post) {
+    return Image.network(
+      "https://api.adorable.io/avatars/500/${post.userId}",
+      fit: BoxFit.cover,
+      height: 300,
+    );
   }
 }
--- lib/widget/main.dart
+++ lib/widget/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: PostListScreen(Repository()),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
